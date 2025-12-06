4c0727be6ed3b017563494f9f2888a92708c10ff
tauqeer0622 <tauqeer0622@gmail.com>
2025-12-06 10:28:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-def sumop(a):
-    def divop():
-        y=a()
-        return y//2
-    return divop
-@sumop
-def display():
-    return 50
-# def mul(s):
-#     return s ** 2
-x=display()
 
-print(x)
