eb196b8d680bb6ec94df9f009e441a240cc07c25
tauqeer0622 <tauqeer0622@gmail.com>
2025-12-06 10:29:24 +0530

    Update browsing_stack.py

--- browsing_stack.py
+++ browsing_stack.py
@@ -1,12 +1,1 @@
-class Stack:
-    def __init__(self):
-        self.stack=[]
-class Browser_stack(Stack):
-    def __init__(self):
-        super().__init__()
-    def push_url(self,value):
-        self.stack.append(value)
-    def pop_url(self):
-        return self.stack.pop()
-    def show_stack(self):
-        print(self.stack)
+
